80735dd6fa00ccb0a38dae57e7d09d97004adfa9
pq <pq.blogspot@gmail.com>
2017-09-08 09:08:32 +0900

    HM

--- essential-sqlalchemy-2e/src/ch04/test_app.py
+++ essential-sqlalchemy-2e/src/ch04/test_app.py
@@ -7,12 +7,12 @@ from app import get_orders_by_customer
 
 
 class TestApp(unittest.TestCase):
-    cookie_orders = [(u'wlk001', u'cookiemon', u'111-111-1111')]
+    cookie_orders = [('wlk001', 'cookiemon', '111-111-1111')]
     cookie_details = [
-        (u'wlk001', u'cookiemon', u'111-111-1111',
-            u'dark chocolate chip', 2, Decimal('1.00')),
-        (u'wlk001', u'cookiemon', u'111-111-1111',
-            u'oatmeal raisin', 12, Decimal('3.00'))]
+        ('wlk001', 'cookiemon', '111-111-1111',
+            'dark chocolate chip', 2, Decimal('1.00')),
+        ('wlk001', 'cookiemon', '111-111-1111',
+            'oatmeal raisin', 12, Decimal('3.00'))]
 
     @classmethod
     def setUpClass(cls):
